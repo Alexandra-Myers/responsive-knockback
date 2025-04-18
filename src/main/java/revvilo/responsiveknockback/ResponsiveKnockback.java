343051c8b3bf8caa2b6086caf93357ebd4a2358f
revvilo <revvilo@protonmail.com>
2025-04-19 03:46:56 +1200

    Initial Commit

---
+++ src/main/java/revvilo/responsiveknockback/ResponsiveKnockback.java
@@ -0,0 +1,11 @@
+package revvilo.responsiveknockback;
+
+import net.neoforged.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(ResponsiveKnockback.MODID)
+public class ResponsiveKnockback
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "responsiveknockback";
+}
---
+++ src/main/java/revvilo/responsiveknockback/mixins/CallAIStepAfterKnockbackMixin.java
@@ -0,0 +1,20 @@
+package revvilo.responsiveknockback.mixins;
+
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public abstract class CallAIStepAfterKnockbackMixin {
+    @Shadow public abstract void aiStep();
+
+    @Inject(method = "knockback", at = @At("TAIL"), cancellable = true)
+    private void RamAIStepCallIntoKnockbackFunction(double strength, double x, double z, CallbackInfo ci) {
+        if(!ci.isCancelled()) {
+            this.aiStep();
+        }
+    }
+}
