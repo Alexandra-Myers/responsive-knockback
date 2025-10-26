6b00528b9d9ae6d8d2f74bb7860887395582ceee
Alexandra-Myers <bia416cat@gmail.com>
2025-10-26 10:59:24 -0400

    wait we need to go first

--- src/main/java/revvilo/responsiveknockback/mixins/RemoveTrackerTickMixin.java
+++ src/main/java/revvilo/responsiveknockback/mixins/RemoveTrackerTickMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import revvilo.responsiveknockback.ResponsiveKnockback;
 
-@Mixin(value = ServerChunkCache.class, priority = 5000)
+@Mixin(value = ServerChunkCache.class, priority = 1)
 public class RemoveTrackerTickMixin {
 	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ChunkMap;tick()V"))
 	public void removeCall(ChunkMap instance, Operation<Void> original) {
